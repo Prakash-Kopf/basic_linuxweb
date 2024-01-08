2a66563030f9d051771689f6f0b35e80ce111d85
PrakashKopf <90958926+Prakash-Kopf@users.noreply.github.com>
2024-01-08 14:02:11 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /var/www/html/basic_linuxweb
-# Run git pull, git push, and restart apache, logging both standard output and errors to /var/www/logs/script.log
-/usr/bin/sudo git pull origin main >> /var/www/logs/script.log 2>&1
-/usr/bin/sudo git push origin main >> /var/www/logs/script.log 2>&1
-/bin/sudo systemctl restart apache2 >> /var/www/logs/script.log 2>&1
+sudo git pull origin main
+sudo git push origin main
+sudo systemctl restart apache2
